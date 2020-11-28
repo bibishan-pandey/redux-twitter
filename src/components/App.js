24aa73c16258a3f92609fae87b7be8bd906a4dbb
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2020-11-28 13:31:34 +0545

    style: formatting, missing semi-colons

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        Starter Code
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                Starter Code
+            </div>
+        );
+    }
 }
 
-export default App
+export default App;
