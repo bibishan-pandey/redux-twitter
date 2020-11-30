44dc9910c862065b3bda82daeea4d5a02b0d3f7b
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2020-11-30 14:08:54 +0545

    chore: added redux-devtools-extension and connect app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import "./index.css";
 
@@ -10,7 +11,7 @@ import App from "./components/App";
 import middleware from "./middleware";
 import reducer from "./reducers";
 
-const store = createStore(reducer, middleware);
+const store = createStore(reducer, composeWithDevTools(middleware));
 
 const Root = () => {
   return (
