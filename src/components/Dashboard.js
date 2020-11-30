e9b39b99f1bda7e71476319265d616e5295de3bd
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2020-11-30 18:43:03 +0545

    feat(component): added dashboard component

---
+++ src/components/Dashboard.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class Dashboard extends Component {
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div>
+        <h3 className={"center"}>Your timeline</h3>
+        <ul className={"dashboard-list"}>
+          {this.props.tweetIds.map((id) => (
+            <li key={id}>
+              <div>Tweet Id: {id}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({ tweets }) {
+  return {
+    tweetIds: Object.keys(tweets).sort(
+      (a, b) => tweets[b].timestamp - tweets[a].timestamp
+    ),
+  };
+}
+
+export default connect(mapStateToProps)(Dashboard);
