c6143997bc2bba7dc6755b55dfb0bfdffd893c9d
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2020-11-30 13:34:40 +0545

    feat: added redux createstore with provider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
 import "./index.css";
 
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import reducer from "./reducers";
+
+const store = createStore(reducer);
+
+const Root = () => {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+};
+
+ReactDOM.render(<Root />, document.getElementById("root"));
